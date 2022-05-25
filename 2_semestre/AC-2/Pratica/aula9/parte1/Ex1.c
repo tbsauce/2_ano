3d26fe54d343a347501a93c33ca8ae375154139c
Sauce <telmobelasauce@ua.pt>
2022-05-25 23:43:18 +0100

    new

--- 2_semestre/AC-2/Pratica/aula9/parte1/Ex1.c
+++ 2_semestre/AC-2/Pratica/aula9/parte1/Ex1.c
@@ -43,20 +43,21 @@ void configureAll() {
     TRISBbits.TRISB4 = 1; // RBx digital output disconnected
     AD1PCFGbits.PCFG4= 0; // RBx configured as analog input
     AD1CON1bits.SSRC = 7; // Conversion trigger selection bits: in this
-                            // mode an internal counter ends sampling and
-                            // starts conversion
+                          // mode an internal counter ends sampling and
+                          // starts conversion
+
     AD1CON1bits.CLRASAM = 1; // Stop conversions when the 1st A/D converter
-     // interrupt is generated. At the same time,
-     // hardware clears the ASAM bit
+                            // interrupt is generated. At the same time,
+                            // hardware clears the ASAM bit
     AD1CON3bits.SAMC = 16; // Sample time is 16 TAD (TAD = 100 ns)
     AD1CON2bits.SMPI = num-1; // Interrupt is generated after XX samples
                             // (replace XX by the desired number of
                             // consecutive samples)
-    AD1CHSbits.CH0SA = 4; // replace x by the desired input
-     // analog channel (0 to 15)
-    AD1CON1bits.ON = 1; // Enable A/D converter
-     // This must the last command of the A/D
-     // configuration sequence 
+    AD1CHSbits.CH0SA = 4;   // replace x by the desired input
+                            // analog channel (0 to 15)
+    AD1CON1bits.ON = 1;     // Enable A/D converter
+                            // This must the last command of the A/D
+                            // configuration sequence 
 
     // Enable interrupts ADC, T1, T3
     IPC6bits.AD1IP = 2;         // configure priority of A/D interrupts
