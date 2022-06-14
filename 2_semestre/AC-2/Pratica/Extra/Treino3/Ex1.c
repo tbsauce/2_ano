f0e49ddb83fb43189c9c0f49bf0d359966eaf722
Sauce <telmobelasauce@ua.pt>
2022-06-14 23:33:39 +0100

    e mais merda

--- 2_semestre/AC-2/Pratica/Extra/Treino3/Ex1.c
+++ 2_semestre/AC-2/Pratica/Extra/Treino3/Ex1.c
@@ -1,46 +1,42 @@
 #include <detpic32.h>
 
-void configureAll();
-void delay(int ms);
-
 volatile int duty = 1;
+void delay(int ms);
+void set(int num);
 
 int main(void){
-
-    while(1){
-
-        configureAll();
-
-        if(PORTB & 0x0009 == 0x0008)
-        duty = 70;
-
-        if(PORTB & 0x0009 == 0x0001)
-        duty = 25;
-
-        delay(25);
-    };
-
-    return 0;
+	
+	//port
+	TRISB = TRISB | 0x0009;
+	
+	//TIMER
+	T2CONbits.TCKPS = 1;
+	PR2 = 35713;
+	TMR2 = 0;
+	T2CONbits.TON = 1;	
+	
+	//OC2
+	OC2CONbits.OCM = 6;
+	OC2CONbits.OCTSEL =0;
+	OC2RS = ((PR2 +1) * duty )/100;
+	OC2CONbits.ON = 1;
+	
+	while(1){
+		if((PORTB & 0x0009) == 0x0001)
+			set(25);
+		if((PORTB & 0x0009) == 0x0008)
+			set(70);
+		
+		delay(5000);		//250us
+	}
+	
+}
+void delay(int ms){
+	resetCoreTimer();
+	while(readCoreTimer() < ms);
 }
 
-void configureAll(){
-    //Port
-    TRISB = TRISB | 0x0009;
-
-    T2CONbits.TCKPS = 1;        //20000000 /((65535 +1) * 280);
-                                //1:2 -> 1
-    PR2 = 35713;                //20000000/(2 *280)- 1 = 35713
-    TMR2 = 0;
-    T2CONbits.TON = 1;
-
-    OC2CONbits.OCM = 6;
-    OC2CONbits.OCTSEL = 0;
-    OC2RS = ((PR2 + 1) * duty) /100;
-    OC2CONbits.ON = 1;
-
-}
-
-void delay(int ms) {
-    resetCoreTimer();
-    while(readCoreTimer() < 200 * ms);
+void set(unsigned int num){
+	if(duty >= 0 && duty <= 100)
+		OC2RS = ((PR2 +1) * duty )/100;
 }
--- 2_semestre/AC-2/Pratica/Extra/Treino3/Ex3.c
+++ 2_semestre/AC-2/Pratica/Extra/Treino3/Ex3.c
@@ -1,94 +1,78 @@
 #include <detpic32.h>
 
-void caseP();
-void caseT();
 void putstr(char *s);
 void putc(char byte);
 
-void configureAll();
-
-
-int main(void){	
-
-	configureAll();
+int main(void){
 	
-	EnableInterrupts();
-	
-	while(1);
-			
-	return 0;	
-	
-}
-
-void _int_(32) isr_uart2 (void){
-	if(IFS1bits.U2RXIF == 1){
-		char byte = U2RXREG;
-		if(byte == 'p')
-			caseP();
-		if(byte == 't')
-			caseT();
-
-		IFS1bits.U2RXIF = 0;	
-	}
-	
-	
-}
-
-void configureAll(){
-
-	//Port
-	TRISB = TRISB | 0x000F;
-	TRISE = TRISE & 0xFFEF;
+	//REset
 	LATE = (LATE & 0xFFEF) | 0x0010;
 	
-	U2BRG = 129;				//(20000000 / (16 * 9600)) - 1
-	U2MODEbits.BRGH = 0;		//16 because yes
-	U2MODEbits.PDSEL = 2;		//8 data odd parity -> 10
-	U2MODEbits.STSEL = 1;		//2 STOP bits
+	//port
+	TRISB = TRISB | 0x000F;
+	TRISE = TRISE & 0xFFEF;
+	
+	//UART2
+	U2BRG= 129;
+	U2MODEbits.BRGH = 0;
+
+	U2MODEbits.PDSEL = 2;
+	U2MODEbits.STSEL = 1;
 	
 	U2STAbits.UTXEN = 1;
-	U2STAbits.URXEN = 1;
+	U2STAbits.UTXEN = 1;
 	
-	U2MODEbits.ON = 1;	
+	U2MODEbits.ON = 1;
 	
-	
+	//UART2 INTERRUPT
 	IEC1bits.U2RXIE = 1;
 	IEC1bits.U2TXIE = 0;
-	
 	IPC8bits.U2IP = 2;
 	IFS1bits.U2RXIF = 0;
 	
 	U2STAbits.URXISEL = 0;
-
 	
+
+	EnableInterrupts();
+
+	while(1);
+	
+	return 0;
+}	
+void _int_ (32) isr_uart2 (void){
+
+	if(IFS1bits.U2RXIF == 1){
+		char byte = U2RXREG;
+		
+		if(byte == 'p'){
+			putstr("DipSwitch=");
+			putc(PORTBbits.RB3 + 0x30);
+			putc(PORTBbits.RB2 + 0x30);
+			putc(PORTBbits.RB1 + 0x30);
+			putc(PORTBbits.RB0 + 0x30);
+		}
+		
+		if(byte == 't')
+			LATE = LATE ^ 0x0010;
+		
+		IFS1bits.U2RXIF = 0;
+	}
 }
 
-void caseP(){
-	putstr("DipSwitch=");
-	putc(PORTBbits.RB3 + 0x30);
-	putc((PORTBbits.RB2) + 0x30);
-	putc((PORTBbits.RB1) + 0x30);
-	putc((PORTBbits.RB0) + 0x30);
-
+void putc(char byte){
+	// wait while UTXBF == 1
+	while(U2STAbits.UTXBF == 1);
+	// Copy byte2send to the UxTXREG register
+	U2TXREG = byte;
 }
-
-void caseT(){
-	LATE = LATE ^ 0x0010;
-}
-
-
-
-
 
 void putstr(char *s){
+
 	while(*s != '\0'){
 		putc(*s);
 		s++;
 	}
-}
-void putc(char byte){
-	while(U2STAbits.UTXBF == 1);
-			U2TXREG = byte;
+	
 }
 
 
